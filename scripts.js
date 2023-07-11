7620e54b13fb8b3962d8097e86cae03d39a0fb0d
Max Nussenbaum <max@maxnuss.com>
2023-07-11 17:36:59 -0400

    remove jquery cruft

--- scripts.js
+++ scripts.js
@@ -1,14 +1,3 @@
-// Load Jquery
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.6.3.min.js'; // Check https://jquery.com/ for the current version
-document.getElementsByTagName('head')[0].appendChild(script);
-
-// Test
-$( document ).ready(function() {
-	console.log('Custom scripts loaded');
-});
-
 document.addEventListener("DOMContentLoaded", function() {
-  // Code to be executed when the DOM is ready
-  console.log("Booyah baby");
+  console.log("Custom scripts are working");
 });
