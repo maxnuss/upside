a38adf00f9d11e59a67f79094c34f2652a2ae375
Max Nussenbaum <max@maxnuss.com>
2023-07-11 17:19:49 -0400

    Add jquery and test console log

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-/* nothing here yet */
+// Load Jquery
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.6.3.min.js'; // Check https://jquery.com/ for the current version
+document.getElementsByTagName('head')[0].appendChild(script);
+
+// Test
+$( document ).ready(function() {
+	console.log('Custom scripts loaded');
+});
